2f34cd57c9af1b4656ea22551f6e99763bf1c702
Dwi Anggun <48617411+rhapsody16@users.noreply.github.com>
2022-12-08 20:03:22 +0700

    Create a dbt project

---
+++ models/stg_vendors.sql
@@ -0,0 +1,4 @@
+select
+    VendorID as vendor_id,
+
+from `nytaxi`.yellow_tripdata_2019
---
+++ seeds/table_joined.sql
@@ -0,0 +1,40 @@
+with table_joined as (
+    SELECT * 
+    FROM {{ source('nytaxi', 'yellow_tripdata_2019-01') }}
+    UNION ALL 
+    SELECT * 
+    FROM {{ source('nytaxi', 'yellow_tripdata_2019-02') }}
+    UNION ALL 
+    SELECT * 
+    FROM {{ source('nytaxi', 'yellow_tripdata_2019-03') }}
+    UNION ALL 
+    SELECT * 
+    FROM {{ source('nytaxi', 'yellow_tripdata_2019-04') }}
+    UNION ALL 
+    SELECT * 
+    FROM {{ source('nytaxi', 'yellow_tripdata_2019-05') }}
+    UNION ALL 
+    SELECT * 
+    FROM {{ source('nytaxi', 'yellow_tripdata_2019-06') }}
+    UNION ALL 
+    SELECT * 
+    FROM {{ source('nytaxi', 'yellow_tripdata_2019-07') }}
+    UNION ALL 
+    SELECT * 
+    FROM {{ source('nytaxi', 'yellow_tripdata_2019-08') }}
+    UNION ALL 
+    SELECT * 
+    FROM {{ source('nytaxi', 'yellow_tripdata_2019-09') }}
+    UNION ALL 
+    SELECT * 
+    FROM {{ source('nytaxi', 'yellow_tripdata_2019-10') }}
+    UNION ALL 
+    SELECT * 
+    FROM {{ source('nytaxi', 'yellow_tripdata_2019-11') }}
+    UNION ALL 
+    SELECT * 
+    FROM {{ source('nytaxi', 'yellow_tripdata_2019-12') }}
+    UNION ALL 
+)
+
+SELECT * FROM table_joined
